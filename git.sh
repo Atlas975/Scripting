207dea1a4e8b86e267658e9a4e308a203bfb7b7b
Atlas975 <adilw2002@gmail.com>
2022-07-04 15:02:25 +0400

    added git script

---
+++ git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+gitTotal() {
+    git add .
+    git commit -m "$*"
+    git push
+}
+gitAuto() {
+    git add .
+    git commit -m "Auto commit"
+    git push
+}
+
+gitNew(){
+    git clone "git@github.com:Atlas975/$*"
+    cd $*
+    git remote set-url origin https://github.com/Atlas975/$*
+    clear
+    echo -e "cloned and linked repository\e[1;32m $*"
+}
+
+alias pgit=gitPush;
+alias agit=gitAuto;
+alias ngit=gitNew;
+alias cgit="git clone $*"
