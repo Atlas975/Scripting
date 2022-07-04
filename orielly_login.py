feacf3c4d6f5abebe7cc010df88b766b45155af0
Atlas975 <adilw2002@gmail.com>
2022-07-05 03:21:27 +0400

    added orielly script

--- orielly_login.py
+++ orielly_login.py
@@ -6,5 +6,5 @@ orielly_bot.click('a','orm-Link-root listedLink--QAwiL ')
 details={'a.wazeer@lancaster.ac.uk':orielly_bot.xml_format('input','orm-Input-input input--f2LI6 ')}
 orielly_bot.enter_text(**details)
 orielly_bot.click('button','orm-Button-root btn--MRgqb ')
-orielly_bot.driver.implicitly_wait(10)
+orielly_bot.wait()
 orielly_bot.click('a','orm-Button-root successBtn--FLaz_  orm-Button-link')
