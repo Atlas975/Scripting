5cff5b2106c085cbf482172efd70c8ab9c966736
Atlas975 <adilw2002@gmail.com>
2023-01-09 17:35:09 +0400

    Auto commit 13:35:09-01/09/23 (UTC)

--- config/gitalias.sh
+++ config/gitalias.sh
@@ -12,7 +12,10 @@ git config --global alias.ps push
 git config --global alias.cl clone
 git config --global alias.in init
 git config --global alias.rm remove
+git config --global alias.ss stash
 git config --global alias.lo log
+
+
 
 # macro aliases
 git_msg_push() {
@@ -27,15 +30,21 @@ git_auto_push() {
     git push
 }
 
-remote_personal() {
+git_remote_personal() {
     git remote set-url origin git@github.com:Atlas975/"$*".git
 }
+
+git_revert_commit() {
+    git revert "$*"
+}
+
 
 alias g-cam="git commit -am"
 alias g-chb="git checkout -b"
 alias g-org="git remote set-url origin"
-alias g-atls=remote_personal
+alias g-atls=git_remote_personal
 alias g-mps=git_msg_push
 alias g-aps=git_auto_push
+alias g-rev=git_revert_commit
 
 
