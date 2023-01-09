3e9a0f51f1f9b72746ebb84aed4ba2d24845564e
Atlas975 <adilw2002@gmail.com>
2023-01-09 12:54:05 +0400

    added git aliases

--- gi.py
+++ gi.py
@@ -1,20 +1,66 @@
 import subprocess as sp
-import sys
+import argparse as ap
+
+
+""" aliases for .config file
+alias
+
+"""
+
+
+class GitParser:
+    def __init__(self) -> None:
+        self.user = "Atlas975"
+        self.remote = f"git@github.com:{self.user}"
+
+        parser = ap.ArgumentParser(prog="gi", usage="gi COMMAND [ARGS ...]")
+        parser.add_argument("CMD", help="git command")
+        parser.add_argument("ARGS", nargs="*", help="git command arguments")
+        self.args = parser.parse_args()
+
+        if not self.args.CMD:
+            raise ValueError(self.args.USAGE)
 
 
 
 
 
+    def alias_match(self, command: str) -> str:
+        match command:
+            case "ad":
+                return "add"
+            case "st":
+                return "status"
+            case "ch":
+                return "checkout"
+            case "ci":
+                return "commit"
+            case "br":
+                return "branch"
+            case "pl":
+                return "pull"
+            case "ps":
+                return "push"
+            case "cl":
+                return "clone"
+            case "in":
+                return "init"
+            case "rm":
+                return "remove"
+            case "lo":
+                return "log"
+            case "me":
+                return "merge"
+            case "re":
+                return "reset"
+            case "di":
+                return "diff"
+            case "
+        return command
 
 
-# print the results of git remote -v
-print(sp.run(["git", "remote", "-v"], capture_output=True).stdout.decode())
 
-
-def main(argv: list[str]) -> int:
-    if len(argv) < 2:
-        raise ValueError("No command provided")
-
+if __name__ == "__main__":
 
 
 
@@ -33,4 +79,4 @@ def main(argv: list[str]) -> int:
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    gp = GitParser()
