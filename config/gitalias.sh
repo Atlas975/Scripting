a697205b980f35d458b7c49c41c60dd2219cfb7f
Atlas975 <adilw2002@gmail.com>
2023-01-09 15:04:35 +0400

    added remote origin alias

---
+++ config/gitalias.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export gbase="git@github.com:Atlas975"
+# general aliases
+git config --global alias.ad add
+git config --global alias.st status
+git config --global alias.ch checkout
+git config --global alias.ro remote
+git config --global alias.co commit
+git config --global alias.br branch
+git config --global alias.pl pull
+git config --global alias.ps push
+git config --global alias.cl clone
+git config --global alias.in init
+git config --global alias.rm remove
+git config --global alias.lo log
+
+# macro aliases
+git config --global alias.cam "commit -am"
+git config --global alias.chb "checkout -b"
+git config --global alias.org "remote set-url origin"
+# git config --global alias.ordrm
+
+
